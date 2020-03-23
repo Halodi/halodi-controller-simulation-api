a1ee74d84cc4d2ea6b313507cd27e868503949b6
Jesper Smith <jesper@halodi.com>
2020-03-23 18:47:27 +0100

    Created ROS2 launch file

--- halodi-controller-gazebo/launch/halodi-controller-gazebo.launch.py
+++ halodi-controller-gazebo/launch/halodi-controller-gazebo.launch.py
@@ -1,12 +1,32 @@
-from launch import LaunchDescription
-import launch_ros.actions
 
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import ThisLaunchFileDir
+
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+    gazebo_package_prefix = get_package_share_directory('gazebo_ros')
+    package_prefix = get_package_share_directory('halodi-controller-gazebo')
+    
     return LaunchDescription([
-        launch_ros.actions.Node(
-            # the name of the executable is set in CMakeLists.txt, towards the end of
-            # the file, in add_executable(...) and the directives following it
-            package='ros2_cpp_pkg', node_executable='cpp_code', output='screen'),
+        
+        DeclareLaunchArgument(
+            'world',
+            default_value=[package_prefix, '/worlds/eve_flat_ground.world'],
+            description='Set world to laucnh',
+            ),
+        
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([gazebo_package_prefix, '/launch/gzserver.launch.py'])
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([gazebo_package_prefix, '/launch/gzclient.launch.py'])
+        ),
     ])
