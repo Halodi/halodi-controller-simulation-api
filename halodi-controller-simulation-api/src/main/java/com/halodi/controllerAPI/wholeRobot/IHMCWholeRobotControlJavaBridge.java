6095178c4a3c1a61a270bd03bb20910a0b0592be
Jesper Smith <jesper@halodi.com>
2020-03-27 15:48:47 +0100

    Removed unused files

--- halodi-controller-simulation-api/src/main/java/com/halodi/controllerAPI/wholeRobot/IHMCWholeRobotControlJavaBridge.java
+++
@@ -1,8 +0,0 @@
-package com.halodi.controllerAPI.wholeRobot;
-
-import com.halodi.controllerAPI.NativeHalodiControllerJavaBridge;
-
-public abstract class IHMCWholeRobotControlJavaBridge extends NativeHalodiControllerJavaBridge
-{
-
-}
