0376fda10a47efc96f9cd54bbe2d381bf83c796a
Nicholas Nadeau <nicholas@halodi.com>
2022-02-22 10:16:17 +0000

    Style: isort imports

--- halodi-controller-gazebo/launch/halodi-controller-gazebo.launch.py
+++ halodi-controller-gazebo/launch/halodi-controller-gazebo.launch.py
@@ -1,16 +1,15 @@
 import os
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
-from launch.actions import SetEnvironmentVariable
-from launch.conditions import IfCondition, UnlessCondition
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch.substitutions import ThisLaunchFileDir
 
 from ament_index_python.packages import get_package_share_directory
-
-
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    SetEnvironmentVariable,
+)
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, ThisLaunchFileDir
 from scripts import GazeboRosPaths
 
 
